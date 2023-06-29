87e8bde6da3d19897426cd4dc25e09f90c597087
Leonides Fernando de Oliveira <leonides.fernando@gmail.com>
2023-06-29 10:05:19 +0200

    improving cucumber reporting generation

--- cucumber.cypress.report.js
+++ cucumber.cypress.report.js
@@ -7,7 +7,7 @@ const baseReportPath = './cypress/cucumber-report';
 
 const data = fs.readFileSync(`${baseReportPath}/results.json`, { encoding: 'utf8', flag: 'r' });
 const runInfo = JSON.parse(data);
-console.log(runInfo)
+
 
 const osName = () => {
   switch (runInfo['osName']) {
@@ -21,25 +21,29 @@ const osName = () => {
       console.log('Undefined browser');
   }
 };
+/*
+ajsutar o path dos arquivos json e message e script pra limpar
+https://github.com/burakkaygusuz/cypress-cucumber-html-report-example/blob/master/package.json
+https://github.com/burakkaygusuz/cypress-cucumber-html-report-example
+https://github.com/burakkaygusuz/cypress-cucumber-html-report-example/blob/master/cypress.report.js
+https://github.com/burakkaygusuz/cypress-cucumber-html-report-example/blob/master/cypress.config.js
+
+
+https://fireflysemantics.medium.com/delete-folder-with-npm-script-12e8e3658b77
+*/
 
 report.generate({
   jsonDir: `${baseReportPath}/json`,
   reportPath: baseReportPath,
-
-});
-/*
-report.generate({
-  jsonDir: baseReportPath,
-  reportPath: baseReportPath,
   metadata: {
     browser: {
-      name: 'chrome',
-      version: 'last',
+      name: runInfo['browserName'],
+      version: runInfo['browserVersion'],
     },
     device: 'Local Test Machine',
     platform: {
       name: osName(),
-      version: 'nao sei',
+      version: runInfo['osVersion'],
     },
   },
   customData: {
@@ -47,15 +51,15 @@ report.generate({
     data: [
       { label: 'Project', value: 'Simple Webb App' },
       { label: 'Release', value: '1.0.0' },
-      { label: 'Cypress Version', value: '12.15' },
-      { label: 'Node Version', value: 'v18.13.0' },
+      { label: 'Cypress Version', value: runInfo['cypressVersion'] },
+      { label: 'Node Version', value: runInfo['nodeVersion'] },
       {
         label: 'Execution Start Time',
-        
+        value: dayjs(runInfo['startedTestsAt']).format('YYYY-MM-DD HH:mm:ss.SSS'),
       },
       {
         label: 'Execution End Time',
-        
+        value: dayjs(runInfo['endedTestsAt']).format('YYYY-MM-DD HH:mm:ss.SSS'),
       },
     ],
   },
@@ -63,4 +67,4 @@ report.generate({
   pageTitle: "Cypress Cucumber Html Report - LFO's lab",
   openReportInBrowser: true,
   displayDuration: true,
-});*/
+});
